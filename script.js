f0e3bd4215c8eff6393ba73aee86c7eb7ddc7b7a
Ulrich.Rohloff <Ulrich.Rohloff@aerosoft.de>
2024-12-09 15:10:17 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const categoryButtons = document
   .getElementById("categoryButtons")
+  .getElementsByClassName("answer");
+const answerButtons = document
+  .getElementById("answerButtons")
   .getElementsByTagName("button");
 const mainMenu = document.getElementById("main-menu");
 const quizMenu = document.getElementById("quiz-menu");
+const quizQuestion = document.getElementById("quiz-question");
+const headerImg = document.getElementById("header-img");
 let json = null;
 let currentQuizData = null;
-
+let quizLength = null;
+let currentQuestionNumber = null;
+console.log("test");
 fetch("./data.json")
   .then((response) => {
     if (!response.ok) {
@@ -20,46 +27,65 @@ fetch("./data.json")
     console.error("Fehler beim Laden der Daten:", error);
   });
 
+
+
 for (let element of categoryButtons) {
   element.addEventListener("click", function (event) {
     category(event);
   });
 }
+for (let element of answerButtons) {
+  element.addEventListener("click", function (event) {
+    answer(event);
+  });
+}
 
 function category(event) {
-  
+  console.log(event.target.id);
   switch (event.target.id) {
     case "SELECT-HTML":
       preloadQuiz("HTML");
+      headerImg.className = "";
+      headerImg.classList.add("html");
 
       break;
 
     case "SELECT-CSS":
       preloadQuiz("CSS");
+      headerImg.className = "";
+      headerImg.classList.add("css");
 
       break;
-      case "SELECT-JS":
-        preloadQuiz("JavaScript");
-  
-        break;
-        case "SELECT-ACCESSIBILITY":
-          preloadQuiz("Accessibility");
-    
-          break;
+    case "SELECT-JS":
+      preloadQuiz("JavaScript");
+
+      headerImg.className = "";
+      headerImg.classList.add("javascript");
+
+      break;
+    case "SELECT-ACCESSIBILITY":
+      preloadQuiz("Accessibility");
+
+      headerImg.className = "";
+      headerImg.classList.add("accessibility");
+
+      break;
   }
 }
 
 function preloadQuiz(title) {
-    for(const key in json["quizzes"]){
-      
-      if (json["quizzes"][key]["title"] == title){
-        currentQuizData = json["quizzes"][key];
-        console.log(currentQuizData);
-      }
+  for (const key in json["quizzes"]) {
+    if (json["quizzes"][key]["title"] == title) {
+      currentQuizData = json["quizzes"][key];
+      console.log(currentQuizData["icon"]);
     }
-  
   }
-
-function loadQuiz(data,questionNumber){
-  
+  headerImg.src = currentQuizData["icon"];
+  quizLength = currentQuizData["questions"].length;
+  console.log(quizLength);
+  mainMenu.classList.toggle("notactive");
+  quizMenu.classList.toggle("notactive");
 }
+
+function loadQuiz(data, questionNumber) {}
+function answer(event) {}
